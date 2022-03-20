28496fec9a9c6c8e3bc40fb3b16c871ce5df929a
ashmi1824 <81317817+ashmi1824@users.noreply.github.com>
2022-03-20 20:31:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var first_name ="Emma";
+console.log(first_name);
+
+var last_name  ="Watson";
+console.log(last_name);
+
+var a ="15";
+console.log(a);
+
+var b ="35";
+console.log(b);
+
+var add ="15+35";
+console.log(add);
+
+var sum ="50";
+console.log(sum);
